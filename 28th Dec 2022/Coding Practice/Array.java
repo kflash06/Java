c1ad1c86de8729e2976403ca047835af24f494ce
kflash06 <ks27543926@gmail.com>
2022-12-28 19:34:28 +0530

    Array coding practice

---
+++ 28th Dec 2022/Coding Practice/Array.java
@@ -0,0 +1,14 @@
+import java.util.Arrays; 
+public class Array {
+	public static void main(String args[]) {
+	int[] age = {12, 14, 22, 9, 21};
+	age[4] = 3;
+	System.out.println("new array " + age);
+	System.out.println("Age of ketan is :" + age[2]);
+	System.out.println(Arrays.toString(age));
+	//using for loop
+	// for (int i = 0; i < age.length; i++) {
+	// 	System.out.println("Age of ketan is" + age[i]);
+	// }
+}
+}
