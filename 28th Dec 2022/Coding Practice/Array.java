0eb0d8a009b4f066a099261e03ed68060c486d4f
kflash06 <ks27543926@gmail.com>
2022-12-28 20:50:08 +0530

    Refactored Array code

--- 28th Dec 2022/Coding Practice/Array.java
+++ 28th Dec 2022/Coding Practice/Array.java
@@ -3,12 +3,12 @@ public class Array {
 	public static void main(String args[]) {
 	int[] age = {12, 14, 22, 9, 21};
 	age[4] = 3;
-	System.out.println("new array " + age);
 	System.out.println("Age of ketan is :" + age[2]);
 	System.out.println(Arrays.toString(age));
 	//using for loop
-	// for (int i = 0; i < age.length; i++) {
-	// 	System.out.println("Age of ketan is" + age[i]);
-	// }
-}
+	 for (int i = 0; i < age.length; i++) {
+	 	System.out.println(age[i]);
+	}
+	
+    }
 }
