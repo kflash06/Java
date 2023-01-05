c3d8e48cf981474c1717a0ff2f261406403f0caa
kflash06 <ks27543926@gmail.com>
2023-01-05 21:33:22 +0530

    Calendar

---
+++ 5th jan 2023/Calendar/src/DaysInMonth.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class DaysInMonth {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the months Number");
+        int n = sc.nextInt();
+        String monthDays;
+        switch (n) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12: monthDays = "31 Days";
+            break;
+            case 4: case 6: case 9: case 11: monthDays = "30 Days";
+            break;
+            case 2:
+                monthDays = "28 days";
+                break;
+
+            default:
+                monthDays = "0 days";
+                break;
+
+    }
+        System.out.println(monthDays);
+    }
+}
---
+++ 5th jan 2023/Calendar/src/Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Months number");
+        int n = sc.nextInt();
+        String month;
+        switch (n) {
+            case 1:
+                month = "January";
+                break;
+            case 2:
+                month = "February";
+                break;
+            case 3:
+                month = "March";
+                break;
+            case 4:
+                month = "April";
+                break;
+            case 5:
+                month = "May";
+                break;
+            case 6:
+                month = "June";
+                break;
+            case 7:
+                month = "July";
+                break;
+            case 8:
+                month = "August";
+                break;
+            case 9:
+                month = "September";
+                break;
+            case 10:
+                month = "october";
+                break;
+            case 11:
+                month = "november";
+                break;
+            case 12:
+                month = "December";
+                break;
+            default:
+                month = "There are 12 months in a calendar year, choose between 1 - 12";
+                break;
+        }
+        System.out.println(month);
+    }
+}
