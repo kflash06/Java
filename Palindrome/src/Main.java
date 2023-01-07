87f32b89d0a5c8991dc031ba2c22a220420f3faa
Ketan Jitendra Shinde <81430421+kflash06@users.noreply.github.com>
2023-01-07 23:51:16 +0530

    Checked Palindrome if true

--- Palindrome/src/Main.java
+++ Palindrome/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(Palindrome.checkPalindrome("234"));
+        System.out.println(Palindrome.checkPalindrome("121"));
     }
 }
